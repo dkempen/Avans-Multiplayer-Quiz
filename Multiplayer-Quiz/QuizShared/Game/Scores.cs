0de01514cdadd14d0062242f8fd5433f0b6ffefd
dkempen <daan.vankempen2@gmail.com>
2019-03-05 10:53:47 +0100

    [Added] Some Tcp Logic + other stuff

---
+++ Multiplayer-Quiz/QuizShared/Game/Scores.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace QuizShared.Game
+{
+    class Scores
+    {
+        // Scores saved in a list with an int array: 0 is the id, 1 is the score
+        private readonly List<int[]> _scores = new List<int[]>();
+
+        public Scores(List<int> ids)
+        {
+            foreach (int id in ids)
+                _scores.Add(new[] { id, 0 });
+        }
+
+        public void AddToScore(int id, int scoreToAdd)
+        {
+            foreach (int[] score in _scores)
+                if (score[0] == id)
+                {
+                    score[1] += scoreToAdd;
+                    return;
+                }
+        }
+
+        public int GetScore(int id)
+        {
+            foreach (int[] score in _scores)
+                if (score[0] == id)
+                    return score[1];
+            return -1;
+        }
+    }
+}
--- Multiplayer-Quiz/QuizShared/Networking/TcpProtocol.cs
+++ Multiplayer-Quiz/QuizShared/Networking/TcpProtocol.cs
@@ -1,10 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QuizShared.Game;
 
 namespace QuizShared.Networking
 {
-    class TcpProtocol
+    static class TcpProtocol
     {
+        public static string Command = "command";
+        public static string Data = "data";
+
+        #region Helper methods
+        public static string GetCommand(JObject json)
+        {
+            return (string) json[Command];
+        }
+
+        public static string GetData(JObject json)
+        {
+            return (string)json[Data];
+        }
+        #endregion
+
+        #region Send by Host
+        public static JObject QuestionScoresSend(Question question, Scores scores)
+        {
+            return new JObject
+            {
+                {Command, "questionScores"},
+                {Data, new JObject
+                {
+                    "question", JsonConvert.SerializeObject(question),
+                    "scores", JsonConvert.SerializeObject(scores)
+                }}
+            };
+        }
+
+        public static Tuple<Question, Scores> QuestionScoresParse(JObject json)
+        {
+            return new Tuple<Question, Scores>(
+                JsonConvert.DeserializeObject<Question>((string)json[Data]["question"]),
+                JsonConvert.DeserializeObject<Scores>((string)json[Data]["scores"]));
+        }
+
+        public static JObject EndScoresSend(Scores scores)
+        {
+            return new JObject
+            {
+                {Command, "endScores"},
+                {Data, JsonConvert.SerializeObject(scores)}
+            };
+        }
+
+        public static Scores EndScoresParse(JObject json)
+        {
+            return JsonConvert.DeserializeObject<Scores>((string)json[Data]);
+        }
+        #endregion
+
+        #region Send by Client
+        public static JObject TimeSend(int time)
+        {
+            return new JObject
+            {
+                {Command, "time"},
+                {Data, time}
+            };
+        }
+
+        public static int TimeParse(JObject json)
+        {
+            return (int)json[Data];
+        }
+        #endregion
     }
 }
