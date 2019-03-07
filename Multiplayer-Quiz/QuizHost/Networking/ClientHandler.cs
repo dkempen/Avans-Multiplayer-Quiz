73e80a7fee413669393b0ffc18c64e246141b9e9
Unknown <maxvnoordennen@gmail.com>
2019-03-07 16:01:59 +0100

    [Added] Basic clienthandler

--- Multiplayer-Quiz/QuizHost/Networking/ClientHandler.cs
+++ Multiplayer-Quiz/QuizHost/Networking/ClientHandler.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QuizShared.Networking;
 
 namespace Multiplayer_Quiz.Networking
 {
     class ClientHandler
     {
+        private TcpClient client;
+        public int id;
+
+        public ClientHandler(TcpClient client, int id)
+        {
+            this.client = client;
+            this.id = id;
+        }
+
+        public void Write(JObject message)
+        {
+            TcpReadWrite.Write(client, message);
+        }
+
+        public JObject Read()
+        {
+            
+            try
+            {
+                return JsonConvert.DeserializeObject<dynamic>((string)TcpReadWrite.Read(client));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception");
+                return null;
+            }
+        }
     }
 }
